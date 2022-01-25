ce0c3d1607a710460ae149b99194e3ae5312248a
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-01-25 04:45:46 +0300

    Init lua scripts and handlers

---
+++ exports/client.lua
@@ -0,0 +1,8 @@
+----------------------------------------------------------------
+--[[ Resource: Animify Pack
+     Script: exports: client.lua
+     Author: vStudio
+     Developer(s): Tron
+     DOC: 25/01/2022
+     Desc: Client-Sided Exports ]]--
+----------------------------------------------------------------
---
+++ handlers/syncer.lua
@@ -0,0 +1,8 @@
+----------------------------------------------------------------
+--[[ Resource: Animify Pack
+     Script: handlers: syncer.lua
+     Author: vStudio
+     Developer(s): Tron
+     DOC: 25/01/2022
+     Desc: Animation Syncer ]]--
+----------------------------------------------------------------
