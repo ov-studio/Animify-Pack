2d2d12a65c7fff141ff94c05a696efda8160d7b7
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-01-25 05:40:16 +0300

    Update loader.lua

--- [Pack]/animify_pack/handlers/loader.lua
+++ [Pack]/animify_pack/handlers/loader.lua
@@ -6,3 +6,39 @@
      DOC: 25/01/2022
      Desc: Animation Loader ]]--
 ----------------------------------------------------------------
+
+
+-------------------
+--[[ Variables ]]--
+-------------------
+
+local loaderCache = {}
+
+
+--------------------------------------------
+--[[ Functions: Loads/Unloads Animation ]]--
+--------------------------------------------
+
+function loadAnimation(animationPath, loadType)
+
+    if animationPath or (type(animationPath) ~= "string") or not loadType or (type(loadType) ~= "string") then return false end
+
+    if loadType == "load" then
+        if not loaderCache[animationPath] then
+            local animationData = vEngine.animation:load(animationPath)
+            if animationData then
+                loaderCache[animationPath] = animationData
+                return animationData
+            end
+        end
+    elseif loadType == "unload" then
+        if loaderCache[animationPath] then
+            local animationData = loaderCache[animationPath]:unload()
+            loaderCache[animationPath] = nil
+            return true
+        end
+    end
+    return false
+
+end
+
