3e5526d96f6cf01ea516d22e16b39d16e8e1eb7b
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-01-25 04:49:52 +0300

    Init settings

---
+++ settings/shared.lua
@@ -0,0 +1,18 @@
+----------------------------------------------------------------
+--[[ Resource: Animify Pack
+     Script: settings: shared.lua
+     Author: vStudio
+     Developer(s): Tron
+     DOC: 25/01/2022
+     Desc: Shared Settings ]]--
+----------------------------------------------------------------
+
+
+-------------------
+--[[ Variables ]]--
+-------------------
+
+Settings = {
+    frameRate = 30, --Recorded in 60 frames for better details
+    syncDistance = 100 --Streaming distance for animations to be synced within
+}
