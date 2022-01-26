5e93d7b492c78da66856e2edc4c866f5cf36e9e9
Aviril <89867985+Aviril@users.noreply.github.com>
2022-01-26 03:25:46 +0300

    Added param to pass custom frameRate

--- [Pack]/animify_pack/handlers/syncer.lua
+++ [Pack]/animify_pack/handlers/syncer.lua
@@ -12,13 +12,13 @@
 --[[ Functions: Syncs Animation ]]--
 ------------------------------------
 
-function syncAnimation(CElement, animPath)
+function syncAnimation(CElement, animPath, frameRate)
 
     if not CElement or not animPath then return false end
     local CAnimation = AnimLoader.load(animPath, "load")
     if not CAnimation then return false end
 
-    return CElement:setAnimation(CAnimation, Settings.frameRate)
+    return CElement:setAnimation(CAnimation, frameRate or Settings.frameRate)
 
 end
 vEngine.event.add("Animify-Pack:syncAnimation", syncAnimation)
